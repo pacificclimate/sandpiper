db1c9e2d4ac637009361cf502f5f809d152234cd
cairosanders <cairosanders@hotmail.com>
2020-10-09 14:57:33 -0700

    Add EvaluateRule()

--- sandpiper/processes/__init__.py
+++ sandpiper/processes/__init__.py
@@ -1,7 +1,9 @@
 from .wps_resolve_rules import ResolveRules
 from .wps_parser import Parser
+from .wps_evaluate_rules import EvaluateRule
 
 processes = [
     ResolveRules(),
     Parser(),
+    EvaluateRule(),
 ]
