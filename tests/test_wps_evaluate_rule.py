f11749a4ed2cd4cfc809d6890702930d5ca37277
cairosanders <cairosanders@hotmail.com>
2020-10-13 09:45:03 -0700

    Create test for evaluate_rule

---
+++ tests/test_wps_evaluate_rule.py
@@ -0,0 +1,28 @@
+import pytest
+from pkg_resources import resource_filename
+
+from wps_tools.testing import run_wps_process
+from sandpiper.processes.wps_evaluate_rule import EvaluateRule
+
+
+@pytest.mark.parametrize(
+    (
+        "rule",
+        "parse_tree",
+        "variables",
+    ),
+    [
+        (
+            "rule_snow",
+            resource_filename("tests", "data/parse_tree.json"),
+            resource_filename("tests", "data/collected_variables.json"),
+        ),
+    ],
+)
+def test_wps_evaluate_rule(rule, parse_tree, variables):
+    datainputs = (
+        f"rule={rule};"
+        f"parse_tree={parse_tree};"
+        f"variables={variables};"
+    )
+    run_wps_process(EvaluateRule(), datainputs)
