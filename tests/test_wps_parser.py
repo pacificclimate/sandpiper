b8447f1784972f778de04da70a67128683a1797d
cairosanders <cairosanders@hotmail.com>
2020-10-14 13:29:18 -0700

    Add comment about equal signs not wokring in  parser test

--- tests/test_wps_parser.py
+++ tests/test_wps_parser.py
@@ -3,7 +3,7 @@ import pytest
 from wps_tools.testing import run_wps_process
 from sandpiper.processes.wps_parser import Parser
 
-
+# Any logical conditions with "=" do not work in this test for some unknown reason
 @pytest.mark.parametrize(
     ("condition"), ["(temp_djf_iamean_s100p_hist < 5)",],
 )
