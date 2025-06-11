7ecfdd14054a50d19bef8a8ba3ea27e2b6c48cf3
Quintin <quintins@uvic.ca>
2025-06-11 12:34:50 -0700

    Update poetry.lock, remove collections patch from conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,1 @@
-import collections
-import collections.abc
 
-# restore the old names so ce.api.geo can import them
-collections.Set = collections.abc.Set
-collections.Mapping = collections.abc.Mapping
